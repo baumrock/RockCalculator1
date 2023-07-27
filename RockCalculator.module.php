6185446dd326ce92fb46b59078db3582c9a03583
BernhardBaumrock <office@baumrock.com>
2023-07-27 14:36:03 +0200

    add module info bullets

--- RockCalculator.module.php
+++ RockCalculator.module.php
@@ -1,4 +1,7 @@
-<?php namespace ProcessWire;
+<?php
+
+namespace ProcessWire;
+
 /**
  * Adds a calculator to any inputfield in the PW backend
  *
@@ -6,11 +9,13 @@
  * @license Licensed under MIT
  * @link https://www.baumrock.com
  */
-class RockCalculator extends WireData implements Module, ConfigurableModule {
+class RockCalculator extends WireData implements Module, ConfigurableModule
+{
 
   private $assetsLoaded = false;
 
-  public static function getModuleInfo() {
+  public static function getModuleInfo()
+  {
     return [
       'title' => 'RockCalculator',
       'version' => '1.0.6',
@@ -23,7 +28,8 @@ class RockCalculator extends WireData implements Module, ConfigurableModule {
     ];
   }
 
-  public function init() {
+  public function init()
+  {
     $this->wire->addHookBefore("Inputfield::render", $this, "hookRender");
     $this->wire->addHookAfter("Inputfield::renderReadyHook", $this, "loadAssets");
   }
@@ -31,20 +37,22 @@ class RockCalculator extends WireData implements Module, ConfigurableModule {
   /**
    * Add RockCalculator class to inputfield
    */
-  public function hookRender(HookEvent $event) {
+  public function hookRender(HookEvent $event)
+  {
     /** @var Inputfield $field */
     $field = $event->object;
-    if(!$this->isEnabled($field)) return;
+    if (!$this->isEnabled($field)) return;
     $field->attr("data-rockcalculator", $field->hasField->rockcalculator);
   }
 
   /**
    * Is calculator enabled on this field?
    */
-  public function isEnabled($inputfield) {
-    if(!$field = $inputfield->hasField) return false;
-    if(!$field->rockcalculator) return false;
-    if($field->inputType AND $field->inputType != "text") {
+  public function isEnabled($inputfield)
+  {
+    if (!$field = $inputfield->hasField) return false;
+    if (!$field->rockcalculator) return false;
+    if ($field->inputType and $field->inputType != "text") {
       $this->warning("Field $field has inputType={$field->inputType} and should have 'text' to make RockCalculator work!");
     }
     return true;
@@ -55,16 +63,17 @@ class RockCalculator extends WireData implements Module, ConfigurableModule {
    * @param HookEvent $event
    * @return void
    */
-  public function loadAssets(HookEvent $event) {
-    if($this->assetsLoaded) return;
+  public function loadAssets(HookEvent $event)
+  {
+    if ($this->assetsLoaded) return;
     $inputfield = $event->object;
-    if(!$this->isEnabled($inputfield)) return;
+    if (!$this->isEnabled($inputfield)) return;
 
     // load files
     $this->wire->config->scripts->add($this->m('lib/math.min.js'));
     $this->wire->config->scripts->add($this->m('lib/tooltip.js'));
-    $this->wire->config->scripts->add($this->m($this->className.'.js'));
-    $this->wire->config->styles->add($this->m($this->className.'.css'));
+    $this->wire->config->scripts->add($this->m($this->className . '.js'));
+    $this->wire->config->styles->add($this->m($this->className . '.css'));
     $this->assetsLoaded = true;
   }
 
@@ -72,18 +81,32 @@ class RockCalculator extends WireData implements Module, ConfigurableModule {
    * Return url including timestamp (cache buster)
    * @return string
    */
-  public function m($file) {
+  public function m($file)
+  {
     $config = $this->wire->config;
-    if(!is_file($file)) $file = $config->paths($this).$file;
-    $m = "?m=".filemtime($file);
-    return str_replace($config->paths->root, $config->urls->root, $file).$m;
+    if (!is_file($file)) $file = $config->paths($this) . $file;
+    $m = "?m=" . filemtime($file);
+    return str_replace($config->paths->root, $config->urls->root, $file) . $m;
   }
 
   /**
-  * Config inputfields
-  * @param InputfieldWrapper $inputfields
-  */
-  public function getModuleConfigInputfields($inputfields) {
-    return $inputfields;
+   * Config inputfields
+   * @param InputfieldWrapper $inputfields
+   */
+  public function getModuleConfigInputfields($inputfields)
+  {
+    $name = strtolower($this);
+    $inputfields->add([
+      'type' => 'markup',
+      'label' => 'Documentation & Updates',
+      'icon' => 'life-ring',
+      'value' => "<p>Hey there, coding rockstars! 👋</p>
+        <ul>
+          <li><a class=uk-text-bold href=https://www.baumrock.com/modules/$name/docs>Read the docs</a> and level up your coding game! 🚀💻😎</li>
+          <li><a class=uk-text-bold href=https://www.baumrock.com/rock-monthly>Sign up now for our monthly newsletter</a> and receive the latest updates and exclusive offers right to your inbox! 🚀💻📫</li>
+          <li><a class=uk-text-bold href=https://github.com/baumrock/$name>Show some love by starring the project</a> and keep me motivated to build more awesome stuff for you! 🌟💻😊</li>
+          <li><a class=uk-text-bold href=https://paypal.me/baumrockcom>Support my work with a donation</a>, and together, we'll keep rocking the coding world! 💖💻💰</li>
+        </ul>",
+    ]);
   }
 }
